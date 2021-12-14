b684065aca95f0dc1c64de092b2ce7dd87ac9d55
vatsalroot <54887589+vatsalroot@users.noreply.github.com>
2021-12-14 13:33:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
 # For loop to enumerate through list of dns servers
 
-for i in $(<$1); do
-
+for i in $(<$1); do 
         if [[ `timeout 2s dig @$i google.com` == *"NOERROR"* ]]; then
                 echo $i | tee -a resolvers.txt
                 continue
